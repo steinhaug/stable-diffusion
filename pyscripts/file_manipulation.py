ea6818163f74cd03fed54618f8bc201b5be89d24
Kim Steinhaug <steinhaug@gmail.com>
2023-06-21 00:43:49 +0200

    Create file_manipulation.py

---
+++ pyscripts/file_manipulation.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+File manipulation - misc tools for editing files
+Author: steinhaug at gmail dot com
+"""
+import os
+import sys
+from collections.abc import Iterable, Sequence
+
+KILL_CODE_FUNC = '''
+import os; import signal;
+def kill_process(msg):
+    os.kill(os.getpid(), signal.SIGINT)
+'''
+KILL_CODE_BUTTON = '''    kill_name = gr.Textbox(label="Name",visible=False)
+    kill_btn = gr.Button("Colab helper - Kill Running Process");
+    kill_btn.click(fn=kill_process, inputs=kill_name, api_name="kill_process")
+'''
+
+# replacements = int, [int], or [(match_string, insert_string, ins_i)]
+def insertInfrontWhenMatched(file_path, replacements):
+
+    if not os.path.isfile( file_path ):
+        print(f'Error opening file: {file_path}')
+        sys.exit(1)
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    if not isinstance(replacements, Sequence):
+        replacements = [ replacements ]
+
+    for my_object in replacements:
+        if isinstance(my_object, Iterable):
+            match_string, insert_string, ins_i = my_object
+        else:
+            if match_string == 1:
+                match_string = 'with gr.Blocks(analytics_enabled=False) as demo:'
+                insert_string = KILL_CODE_FUNC
+                ins_i = 1
+            elif match_string == 2:
+                match_string = 'if shared.args.listen:'
+                insert_string = KILL_CODE_BUTTON
+                ins_i = 1
+
+        for i, line in enumerate(lines):
+            if match_string in line:
+                lines.insert(i - ins_i, insert_string)
+                break
+
+    with open('_' + file_path, 'w') as file:
+        file.writelines(lines)
+
+
+#if __name__ == '__main__':
+#    main()
